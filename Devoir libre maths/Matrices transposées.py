bcc0bd218ad09301dc37cf67205c0d38ed29f262
Louis Drapier <62361743+louis172000@users.noreply.github.com>
2020-05-20 10:14:32 +0200

    Delete Matrices transposées.py

--- Devoir libre maths/Matrices transposées.py
+++
@@ -1,8 +0,0 @@
-import numpy
-M =(1/(2**0.5))* numpy.array(([1, 1, 0], [0, 0, 2**0.5], [-1, 1, 0]))
-N = numpy.transpose(M)
-
-print("M = \n", M)
-print("Transpose = \n", N)
-print("produit = \n", numpy.dot(M, N))
-
