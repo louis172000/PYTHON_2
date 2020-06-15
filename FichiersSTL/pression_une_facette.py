1c189d8ed172d911abc8fe64ed09be6e6f57f3d0
Louis Drapier <62361743+louis172000@users.noreply.github.com>
2020-06-15 17:08:32 +0200

    Add files via upload

--- FichiersSTL/pression_une_facette.py
+++ FichiersSTL/pression_une_facette.py
@@ -2,20 +2,24 @@
 
 
 def forcePressanteFacette(a,b,c,n):
-    ab = [b[0]-a[0],b[1]-a[1],b[2]-a[2]]
+    if a[0]<=0 and b[0]<=0 and c[0]<=0 :
+        ab = [b[0]-a[0],b[1]-a[1],b[2]-a[2]]
 
-    ac = [c[0]-a[0],c[1]-a[1],c[2]-a[2]]
+        ac = [c[0]-a[0],c[1]-a[1],c[2]-a[2]]
 
-    ab_scalaire_ac = [ab[1]*ac[2]-ab[2]*ac[1],ab[2]*ac[0]-ab[0]*ac[2],ab[0]*ac[1]-ab[1]*ac[0]]
+        ab_scalaire_ac = [ab[1]*ac[2]-ab[2]*ac[1],ab[2]*ac[0]-ab[0]*ac[2],ab[0]*ac[1]-ab[1]*ac[0]]
 
-    normeScalaireSurDeux = (((ab_scalaire_ac[0]**2)+(ab_scalaire_ac[1]**2)+(ab_scalaire_ac[2]**2))**(1/2))/2
+        normeScalaireSurDeux = (((ab_scalaire_ac[0]**2)+(ab_scalaire_ac[1]**2)+(ab_scalaire_ac[2]**2))**(1/2))/2
 
-    surfaceSurNormale = [normeScalaireSurDeux*n[0],normeScalaireSurDeux*n[1],normeScalaireSurDeux*n[2]]
+        surfaceSurNormale = [normeScalaireSurDeux*n[0],normeScalaireSurDeux*n[1],normeScalaireSurDeux*n[2]]
 
-    z = (a[2]+b[2]+c[2])/3
+        z = (a[2]+b[2]+c[2])/3
 
-    rho = 1000
-    g = 9.81
-    forcePressante = [surfaceSurNormale[0]*rho*g*z,surfaceSurNormale[1]*rho*g*z,surfaceSurNormale[2]*rho*g*z]
+        rho = 1000
+        g = 9.81
+        forcePressante = [surfaceSurNormale[0]*rho*g*z,surfaceSurNormale[1]*rho*g*z,surfaceSurNormale[2]*rho*g*z]
+
+    elif a[0]>=0 and b[0]>=0 and c[0]>=0 :
+        forcePressante = [0,0,0]
 
     return forcePressante
