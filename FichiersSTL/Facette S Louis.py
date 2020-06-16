e87fbfec59cb92715852c30ac016f4805fe5b09f
Louis Drapier <62361743+louis172000@users.noreply.github.com>
2020-06-16 10:57:55 +0200

    Add files via upload

--- FichiersSTL/Facette S Louis.py
+++ FichiersSTL/Facette S Louis.py
@@ -1,44 +1,70 @@
 import numpy as np
 
 fichier = open("FichiersSTL/Rectangular_HULL.stl")
-#print(fichier.read())
 
-ligne = fichier.readline()
-list_ligne = ligne.split(" ")
-print(len(list_ligne))
-list_ligne = [0, 0, 0, 0]
+ligne = fichier.readlines()
+del ligne[0]
+del ligne[-1]
+print(ligne)
+nb_triangles = int(len(ligne)/7)
+print(nb_triangles)
 
-# Matrice_triangle = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+"""
+exemple
+solid OBJECT
+  facet normal 0 -1 -0
+    outer loop
+      vertex 0 -1 0
+      vertex 2 -1 0
+      vertex 2 -1 1
+    endloop
+  endfacet
+  facet normal 0 -1 -0
+    outer loop
+      vertex 2 -1 0
+      vertex 4 -1 0
+      vertex 4 -1 1
+    endloop
+  endfacet
+"""
 
-i=3
-j=4
-k=5
-Matrice_triangle = np.array([])
+liste_globale = []
+liste_triangle = []
 
-def coordonnées(ligne, list_ligne, Matrice_triangle):
-    while len(list_ligne) >= 4:
-        ligne = fichier.readline()
-        for i in ligne:
-            list_ligne = ligne.split(" ")
-        print(list_ligne)
-        if list_ligne[2] == "facet":
-            print(1)
-            matrice_blanche = np.zeros(14)
-            Matrice_triangle = np.l_[Matrice_triangle, matrice_blanche]
-            print(Matrice_triangle)
-            co1 = list_ligne[4]
-            co2 = list_ligne[5]
-            co3 = list_ligne[6]
-            Matrice_triangle[0][0] = co1
-            Matrice_triangle[0][1] = co2
-            Matrice_triangle[0][2] = co3
-            print("M = ", Matrice_triangle)
+for iteration in range(0, nb_triangles-1):
+    fin_de_lignenormale = (ligne[0+7*iteration])[15:]
+    fin_de_lignenormale = fin_de_lignenormale.split(" ")
+    for i in range(0, len(fin_de_lignenormale)):
+        fin_de_lignenormale[i] = float(fin_de_lignenormale[i])
+        liste_triangle.append(fin_de_lignenormale[i])
+    print("fin de ligne Normale", fin_de_lignenormale)
 
+    fin_de_ligneA = (ligne[2+7*iteration])[13:]
+    fin_de_ligneA = fin_de_ligneA.split(" ")
+    for i in range(0, len(fin_de_ligneA)):
+        fin_de_ligneA[i] = float(fin_de_ligneA[i])
+        liste_triangle.append(fin_de_ligneA[i])
+    print("fin de ligne A      ", fin_de_ligneA)
 
+    fin_de_ligneB = (ligne[3+7*iteration])[13:]
+    fin_de_ligneB = fin_de_ligneB.split(" ")
+    for i in range(0, len(fin_de_ligneB)):
+        fin_de_ligneB[i] = float(fin_de_ligneB[i])
+        liste_triangle.append(fin_de_ligneB[i])
+    print("fin de ligne B      ", fin_de_ligneB)
 
+    fin_de_ligneC = (ligne[4+7*iteration])[13:]
+    fin_de_ligneC = fin_de_ligneC.split(" ")
+    for i in range(0, len(fin_de_ligneC)):
+        fin_de_ligneC[i] = float(fin_de_ligneC[i])
+        liste_triangle.append(fin_de_ligneC[i])
+    print("fin de ligne C      ", fin_de_ligneC)
+    print("liste du nieme triangle: ", liste_triangle)
 
+    liste_globale.append(liste_triangle)
+    # la liste_triangle à été ajoutée
+    liste_triangle = []
 
-
-coordonnées(ligne, list_ligne, Matrice_triangle)
+print("liste globale :", liste_globale)
 
 
