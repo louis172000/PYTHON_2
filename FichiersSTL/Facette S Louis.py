5129cd371917ed07dc463ca17862dbcdb584830a
Louis Drapier <62361743+louis172000@users.noreply.github.com>
2020-06-16 11:27:35 +0200

    Add files via upload

--- FichiersSTL/Facette S Louis.py
+++ FichiersSTL/Facette S Louis.py
@@ -1,70 +1,56 @@
 import numpy as np
-
 fichier = open("FichiersSTL/Rectangular_HULL.stl")
 
-ligne = fichier.readlines()
-del ligne[0]
-del ligne[-1]
-print(ligne)
-nb_triangles = int(len(ligne)/7)
-print(nb_triangles)
+############################################################################
+#############             Definitions de fonctions             #############
+############################################################################
 
-"""
-exemple
-solid OBJECT
-  facet normal 0 -1 -0
-    outer loop
-      vertex 0 -1 0
-      vertex 2 -1 0
-      vertex 2 -1 1
-    endloop
-  endfacet
-  facet normal 0 -1 -0
-    outer loop
-      vertex 2 -1 0
-      vertex 4 -1 0
-      vertex 4 -1 1
-    endloop
-  endfacet
-"""
 
-liste_globale = []
-liste_triangle = []
+def fonction_coordonnees_globales(liste, liste_triangle):
+    liste = liste.split(" ")
+    for i in range(0, len(liste)):
+        liste[i] = float(liste[i])
+        liste_triangle.append(liste[i])                 # ajoute les coordonnées (du type float) à la liste du triangle en question
+    # print("liste :", liste)
 
-for iteration in range(0, nb_triangles-1):
-    fin_de_lignenormale = (ligne[0+7*iteration])[15:]
-    fin_de_lignenormale = fin_de_lignenormale.split(" ")
-    for i in range(0, len(fin_de_lignenormale)):
-        fin_de_lignenormale[i] = float(fin_de_lignenormale[i])
-        liste_triangle.append(fin_de_lignenormale[i])
-    print("fin de ligne Normale", fin_de_lignenormale)
 
-    fin_de_ligneA = (ligne[2+7*iteration])[13:]
-    fin_de_ligneA = fin_de_ligneA.split(" ")
-    for i in range(0, len(fin_de_ligneA)):
-        fin_de_ligneA[i] = float(fin_de_ligneA[i])
-        liste_triangle.append(fin_de_ligneA[i])
-    print("fin de ligne A      ", fin_de_ligneA)
-
-    fin_de_ligneB = (ligne[3+7*iteration])[13:]
-    fin_de_ligneB = fin_de_ligneB.split(" ")
-    for i in range(0, len(fin_de_ligneB)):
-        fin_de_ligneB[i] = float(fin_de_ligneB[i])
-        liste_triangle.append(fin_de_ligneB[i])
-    print("fin de ligne B      ", fin_de_ligneB)
-
-    fin_de_ligneC = (ligne[4+7*iteration])[13:]
-    fin_de_ligneC = fin_de_ligneC.split(" ")
-    for i in range(0, len(fin_de_ligneC)):
-        fin_de_ligneC[i] = float(fin_de_ligneC[i])
-        liste_triangle.append(fin_de_ligneC[i])
-    print("fin de ligne C      ", fin_de_ligneC)
-    print("liste du nieme triangle: ", liste_triangle)
-
-    liste_globale.append(liste_triangle)
-    # la liste_triangle à été ajoutée
+def creation_liste_globale(ligne):
+    liste_globale = []
     liste_triangle = []
+    for iteration in range(0, nb_triangles-1):
+        fin_de_lignenormale = (ligne[0+7*iteration])[15:]                   # récupere les derniers éléments de la ligne
+        fonction_coordonnees_globales(fin_de_lignenormale, liste_triangle)
 
-print("liste globale :", liste_globale)
+        fin_de_ligne_a = (ligne[2+7*iteration])[13:]
+        fonction_coordonnees_globales(fin_de_ligne_a, liste_triangle)
+
+        fin_de_ligne_b = (ligne[3+7*iteration])[13:]
+        fonction_coordonnees_globales(fin_de_ligne_b, liste_triangle)
+
+        fin_de_ligne_c = (ligne[4+7*iteration])[13:]
+        fonction_coordonnees_globales(fin_de_ligne_c, liste_triangle)
+
+        # print("liste du nieme triangle: ", liste_triangle)
+
+        liste_globale.append(liste_triangle)
+        # la liste_triangle à été ajoutée
+        liste_triangle = []
+
+    print("liste globale :", liste_globale)
+    return liste_globale
+
+############################################################################
+#############                   Main program                   #############
+############################################################################
 
 
+ligne = fichier.readlines()     # chaque élément représente une ligne
+del ligne[0]
+del ligne[-1]                   # supprime le premier et dernier élément extrait du fichier STL
+# print(ligne)
+nb_triangles = int(len(ligne)/7)
+print("Structure composée de :", nb_triangles, "triangles")
+print("""clasée de la façon suivante: liste_globale = [coordonées du triangle1 (type:liste), coordonées du triangle2 (type:liste), ...]
+""")
+
+creation_liste_globale(ligne)
