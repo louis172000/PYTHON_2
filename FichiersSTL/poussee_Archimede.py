6837067f881f09268d2d88f702c9442fb4a16a9e
Louis Drapier <62361743+louis172000@users.noreply.github.com>
2020-06-17 09:58:06 +0200

    Add files via upload

--- FichiersSTL/poussee_Archimede.py
+++ FichiersSTL/poussee_Archimede.py
@@ -1,4 +1,5 @@
 def interieurSommeArchimede(facette):                                                              #Calcul de l'intérieur de la somme de la formule d'Archimède
+
     if facette[5]<=0 and facette[8]<=0 and facette[11]<=0 :                                                        #si la facette est imergée, on calcul la pression de l'eau sur celle-ci
         ab = [facette[6]-facette[3],facette[7]-facette[4],facette[8]-facette[5]]
         ac = [facette[9]-facette[3],facette[10]-facette[4],facette[11]-facette[5]]
@@ -6,7 +7,7 @@ def interieurSommeArchimede(facette):                                           
         normeScalaireSurDeux = (((ab_scalaire_ac[0]**2)+(ab_scalaire_ac[1]**2)+(ab_scalaire_ac[2]**2))**(1/2))/2
         surfaceSurNormale = [normeScalaireSurDeux*facette[0],normeScalaireSurDeux*facette[1],normeScalaireSurDeux*facette[2]]
         z = (facette[5]+facette[8]+facette[11])/3
-        interieurSomme = [surfaceSurNormale[0]*z*9810,surfaceSurNormale[1]*z*9810,surfaceSurNormale[2]*z*9810]
+        interieurSomme = [surfaceSurNormale[0]*z,surfaceSurNormale[1]*z,surfaceSurNormale[2]*z]
         return interieurSomme
 
     elif facette[5]>=0 and facette[8]>=0 and facette[11]>=0 :                                                      #si la facette n'est pas imergée, on ne calcul pas la pression de l'eau sur celle-ci
