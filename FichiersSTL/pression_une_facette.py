ca02355fb0387b00b5ada9d570811a967b05a286
Louis Drapier <62361743+louis172000@users.noreply.github.com>
2020-06-17 20:35:46 +0200

    Add files via upload

--- FichiersSTL/pression_une_facette.py
+++ FichiersSTL/pression_une_facette.py
@@ -1,8 +1,9 @@
-#Calcul de la force de pression sur une facette
-
+#################################################################################
+################  Fonction de la pression sur une facette   #####################
+#################################################################################
 
 def forcePressanteFacette(facette):
-    if facette[5]<=0 and facette[8]<=0 and facette[11]<=0 :                                                        #si la facette est imergée, on calcul la pression de l'eau sur celle-ci
+    if facette[5]<=0 and facette[8]<=0 and facette[11]<=0 :                                                   #si la facette est totalement immergée, on calcul la pression de l'eau sur celle-ci
         ab = [facette[6]-facette[3],facette[7]-facette[4],facette[8]-facette[5]]
         ac = [facette[9]-facette[3],facette[10]-facette[4],facette[11]-facette[5]]
         ab_scalaire_ac = [ab[1]*ac[2]-ab[2]*ac[1],ab[2]*ac[0]-ab[0]*ac[2],ab[0]*ac[1]-ab[1]*ac[0]]
@@ -13,5 +14,6 @@ def forcePressanteFacette(facette):
         g = 9.81
         forcePressante = [surfaceSurNormale[0]*rho*g*z,surfaceSurNormale[1]*rho*g*z,surfaceSurNormale[2]*rho*g*z]
         return forcePressante
-    elif facette[5]>=0 and facette[8]>=0 and facette[11]>=0 :
+
+    elif facette[5]>=0 and facette[8]>=0 and facette[11]>=0 :                                                 #si la facette n'est pas du tout immergée, la pression sur celle-ci est négigeable
         return [0,0,0]
