7e4a138ac9052eda8aae7f550e3876ff6cb61974
Louis Drapier <62361743+louis172000@users.noreply.github.com>
2020-06-19 01:06:03 +0200

    Add files via upload

--- test_fonctions_STL/test_int_somme_Archimede.py
+++ test_fonctions_STL/test_int_somme_Archimede.py
@@ -2,10 +2,34 @@
 ####### Test de la fonction de l'intérieur de la somme de la poussée d'Archimède ########
 #########################################################################################
 
+############## fonction générale ##############
 
-def interieurSommeArchimede(facette):                                                                       #Calcul de l'intérieur de la somme de la formule d'Archimède
+def pointZ0(a,b):
+    if a[2]!= b[2]:
+        xC = ((a[2]*b[0])-(b[2]*a[0]))/(a[2]-b[2])
+        yC = ((a[1]*b[2])-(a[2]*b[1]))/(b[2]-a[2])
+        return [xC,yC,0]
+    else:
+        return [0,0,0]
 
-    if facette[5]<=0 and facette[8]<=0 and facette[11]<=0 :                                                 #si la facette est immergée, on calcul la pression de l'eau sur celle-ci
+
+############## fonction de l'intérieur de la somme de la poussée d'Archimède ##############
+
+def interieurSommeArchimede(facette):
+    if facette[5]>0 and facette[8]>0 and facette[11]<=0 :
+        [facette[3],facette[4],facette[5]] = pointZ0([facette[9],facette[10],facette[11]],[facette[3],facette[4],facette[5]])
+        [facette[6],facette[7],facette[8]] = pointZ0([facette[9],facette[10],facette[11]],[facette[6],facette[7],facette[8]])
+
+    elif facette[5]>0 and facette[8]<=0 and facette[11]>0 :
+        [facette[3],facette[4],facette[5]] = pointZ0([facette[6],facette[7],facette[8]],[facette[3],facette[4],facette[5]])
+        [facette[9],facette[10],facette[11]] = pointZ0([facette[6],facette[7],facette[8]],[facette[9],facette[10],facette[11]])
+
+    elif facette[5]<=0 and facette[8]>0 and facette[11]>0 :
+        [facette[9],facette[10],facette[11]] = pointZ0([facette[3],facette[4],facette[5]],[facette[9],facette[10],facette[11]])
+        [facette[6],facette[7],facette[8]] = pointZ0([facette[3],facette[4],facette[5]],[facette[6],facette[7],facette[8]])
+
+
+    if facette[5]<=0 and facette[8]<=0 and facette[11]<=0 :
         ab = [facette[6]-facette[3],facette[7]-facette[4],facette[8]-facette[5]]
         ac = [facette[9]-facette[3],facette[10]-facette[4],facette[11]-facette[5]]
         ab_scalaire_ac = [ab[1]*ac[2]-ab[2]*ac[1],ab[2]*ac[0]-ab[0]*ac[2],ab[0]*ac[1]-ab[1]*ac[0]]
@@ -15,10 +39,13 @@ def interieurSommeArchimede(facette):                                           
         interieurSomme = [surfaceSurNormale[0]*z,surfaceSurNormale[1]*z,surfaceSurNormale[2]*z]
         return interieurSomme
 
-    elif facette[5]>=0 and facette[8]>=0 and facette[11]>=0 :                                                #si la facette n'est pas immergée, on ne calcul pas la pression de l'eau sur celle-ci
+    else:
         return [0,0,0]
 
 
+################################################
+##############    Main program    ##############
+################################################
 
 #Nous avons pris une facette de façon arbitraire
 objetEtudie = [0,0,1,0,0,-1,1,0,-1,0,1,-1]                             #Après calcul théorique, nous obtenons [0, 0, -0.5]
