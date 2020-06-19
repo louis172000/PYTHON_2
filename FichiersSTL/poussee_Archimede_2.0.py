84177387deca2df298af31cbf6bab94df306d752
Louis Drapier <62361743+louis172000@users.noreply.github.com>
2020-06-19 09:51:22 +0200

    Add files via upload

--- FichiersSTL/poussee_Archimede_2.0.py
+++ FichiersSTL/poussee_Archimede_2.0.py
@@ -2,9 +2,11 @@
 ###################  Fonction de la poussé d'Archimède   ########################
 #################################################################################
 
+#Poussée d'Archimède sur un objet en prenant en compte toutes les parties immergées des facettes
+
 ############### fonctions générales ###############
 
-def pointZ0(a,b):
+def pointZ0(a,b):                                                                   #fonction permettant de trouver le point à z=0 entre un point à z>0 et un point à z<0
     if a[2]!= b[2]:
         xC = ((a[2]*b[0])-(b[2]*a[0]))/(a[2]-b[2])
         yC = ((a[1]*b[2])-(a[2]*b[1]))/(b[2]-a[2])
@@ -13,7 +15,8 @@ def pointZ0(a,b):
         return [0,0,0]
 
 
-def interieurSommeArchimede(facette):
+def interieurSommeArchimede(facette):                                              #Calcul de l'intérieur de la somme de la poussée d'Archimède
+    #Nous réduisons les facettes qui ont deux point à z>0, afin d'avoir une leur partie complétement immergée
     if facette[5]>0 and facette[8]>0 and facette[11]<=0 :
         [facette[3],facette[4],facette[5]] = pointZ0([facette[9],facette[10],facette[11]],[facette[3],facette[4],facette[5]])
         [facette[6],facette[7],facette[8]] = pointZ0([facette[9],facette[10],facette[11]],[facette[6],facette[7],facette[8]])
@@ -27,7 +30,7 @@ def interieurSommeArchimede(facette):
         [facette[6],facette[7],facette[8]] = pointZ0([facette[3],facette[4],facette[5]],[facette[6],facette[7],facette[8]])
 
 
-    if facette[5]<=0 and facette[8]<=0 and facette[11]<=0 :
+    if facette[5]<=0 and facette[8]<=0 and facette[11]<=0 :                         #Calcul de l'intérieur de la somme pour la partie immergé d'une facette
         ab = [facette[6]-facette[3],facette[7]-facette[4],facette[8]-facette[5]]
         ac = [facette[9]-facette[3],facette[10]-facette[4],facette[11]-facette[5]]
         ab_scalaire_ac = [ab[1]*ac[2]-ab[2]*ac[1],ab[2]*ac[0]-ab[0]*ac[2],ab[0]*ac[1]-ab[1]*ac[0]]
@@ -37,15 +40,16 @@ def interieurSommeArchimede(facette):
         interieurSomme = [surfaceSurNormale[0]*z,surfaceSurNormale[1]*z,surfaceSurNormale[2]*z]
         return interieurSomme
 
-    else:
+    else:                                                                           #si la facette n'est pas du tout immergée, la pression sur celle-ci est négligée
         return [0,0,0]
 
 
 ############### fonction de la poussée d'Archimède ###############
 
-def archimede(objet):
+def archimede(objet):                                                               #calcul de la poussée d'Archimède sur un objet composé d'un certain nombre de facette
     somme = [0,0,0]
     for facette2 in objet:
+        #Nous étudions tout d'abord les facettes partiellement immergées qui ont un unique point avec un z>=0
         if facette2[5]>=0 and facette2[8]<0 and facette2[11]<0 :                                                                # A positif
             premierNewPoint  = pointZ0([facette2[3],facette2[4],facette2[5]],[facette2[6],facette2[7],facette2[8]])             # Calcul de B'
             deuxiemeNewPoint = pointZ0([facette2[3],facette2[4],facette2[5]],[facette2[9],facette2[10],facette2[11]])           # Calcul de C'
@@ -73,6 +77,7 @@ def archimede(objet):
             interieurSommeDeuxiemeTriangle = interieurSommeArchimede(deuxiemeNewTriangle)
             somme = [somme[0]+interieurSommePremierTriangle[0]+interieurSommeDeuxiemeTriangle[0], somme[1]+interieurSommePremierTriangle[1]+interieurSommeDeuxiemeTriangle[1], somme[2]+interieurSommePremierTriangle[2]+interieurSommeDeuxiemeTriangle[2]]
 
+        #Nous étudions ensuite toutes les autres facettes
         else:
             interieurSomme = interieurSommeArchimede(facette2)
             somme = [somme[0]+interieurSomme[0], somme[1]+interieurSomme[1], somme[2]+interieurSomme[2]]
