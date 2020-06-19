ebb97e2d716233259b639b32dc7c6eb89e1181cf
Louis Drapier <62361743+louis172000@users.noreply.github.com>
2020-06-19 09:58:37 +0200

    Add files via upload

--- FichiersSTL/pression_une_facette.py
+++ FichiersSTL/pression_une_facette.py
@@ -2,10 +2,11 @@
 ################  Fonction de la pression sur une facette   #####################
 #################################################################################
 
+#Pression de l'eau sur une facette qui est soit complétement immergée, soit qui a deux point à z>0
 
 ############## fonction générale ##############
 
-def pointZ0(a,b):
+def pointZ0(a,b):                                               #fonction permettant de trouver le point à z=0 entre un point à z>0 et un point à z<0
     if a[2]!= b[2]:
         xC = ((a[2]*b[0])-(b[2]*a[0]))/(a[2]-b[2])
         yC = ((a[1]*b[2])-(a[2]*b[1]))/(b[2]-a[2])
@@ -17,6 +18,7 @@ def pointZ0(a,b):
 ############## fonction de la pression sur une facette ##############
 
 def forcePressanteFacette(facette):
+    #Nous réduisons la facette si elle a deux point à z>0, afin d'avoir sa partie complétement immergée
     if facette[5]>0 and facette[8]>0 and facette[11]<=0 :
         [facette[3],facette[4],facette[5]] = pointZ0([facette[9],facette[10],facette[11]],[facette[3],facette[4],facette[5]])
         [facette[6],facette[7],facette[8]] = pointZ0([facette[9],facette[10],facette[11]],[facette[6],facette[7],facette[8]])
@@ -30,7 +32,7 @@ def forcePressanteFacette(facette):
         [facette[6],facette[7],facette[8]] = pointZ0([facette[3],facette[4],facette[5]],[facette[6],facette[7],facette[8]])
 
 
-    if facette[5]<=0 and facette[8]<=0 and facette[11]<=0 :
+    if facette[5]<=0 and facette[8]<=0 and facette[11]<=0 :                                   #Calcul de la partie immergée de la facette
         ab = [facette[6]-facette[3],facette[7]-facette[4],facette[8]-facette[5]]
         ac = [facette[9]-facette[3],facette[10]-facette[4],facette[11]-facette[5]]
         ab_scalaire_ac = [ab[1]*ac[2]-ab[2]*ac[1],ab[2]*ac[0]-ab[0]*ac[2],ab[0]*ac[1]-ab[1]*ac[0]]
@@ -42,5 +44,5 @@ def forcePressanteFacette(facette):
         forcePressante = [surfaceSurNormale[0]*rho*g*z,surfaceSurNormale[1]*rho*g*z,surfaceSurNormale[2]*rho*g*z]
         return forcePressante
 
-    else:
+    else:                                                                           #si la facette n'est pas du tout immergée, la pression sur celle-ci est négligée
         return [0,0,0]
